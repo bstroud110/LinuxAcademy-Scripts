86dfc06381634d90a1f1e7a794ab5aac1d1733cc
Bryan Stroud <bstroud110@gmail.com>
2018-11-02 15:47:06 -0500

    Updated substitution.sh

--- substitution.sh
+++ substitution.sh
@@ -2,11 +2,19 @@
 
 #This script is intended to show how to do simple substitution
 
+shopt -s expand_aliases
+
+alias TODAY="date"
+alias UFILES="find /home -user user"
+
 TODAYSDATE=`date`
 USERFILES=`find /home -user user`
 
 echo "Today's date is:  $TODAYSDATE"
 echo "All files owned by User:  $USERFILES"
 
-
-
+A=`TODAY`
+B=`UFILES`
+echo "With Alias, TODAY is: $A"
+echo "With Alias, UFILES is:  $B"
+ 
