eb70b853cf903cced0f63dd699e93e6fa725cb22
root <root@bstroud1101.mylabserver.com>
2018-11-02 13:18:06 -0500

    Added env.sh for displaying environment variables.

---
+++ env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+
+echo "This script will give us environment information"
+echo "================================================"
+echo ""
+echo "Hello Username:  $USER"
+echo ""
+echo "Your Home directory is:  $HOME"
+echo ""
+echo "Your History file will ignore:  $HISTCONTROL"
+echo ""
+echo "Your terminal session type is:  $TERM"
+echo ""
+
+
