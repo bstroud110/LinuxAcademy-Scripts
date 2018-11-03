d0bdbed305db0f101722ecc0093ef333ea0c7ceb
Bryan Stroud <bstroud110@gmail.com>
2018-11-02 20:55:04 -0500

    Added additional interactive questions to readsample.sh

--- readsample.sh
+++ readsample.sh
@@ -4,5 +4,11 @@
 echo "Enter your first name:  "
 read FIRSTNAME
 
-echo "Hello $FIRSTNAME"
+echo "Enter your last name:  "
+read LASTNAME
+echo "Your full name is $FIRSTNAME $LASTNAME"
+echo
+echo "Enter your age:  "
+read USERAGE
+echo "In 10 years, you will be:  `expr 10 + $USERAGE` years old."
 
