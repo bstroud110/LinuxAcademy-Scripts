c2c512590cc440acfc763aa83716c19df05ca0c5
Bryan Stroud <bstroud110@gmail.com>
2018-11-03 17:33:05 -0500

    Created script to display some environment variables.

---
+++ envvariables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Display USER, HOME, HISTCONTROL, and TERM environment variables to the screen
+
+echo "The USER environment variable is:  $USER"
+echo "The HOME environment variable is:  $HOME"
+echo "The HISTCONTROL environment variable is:  $HISTCONTROL"
+echo "The TERM environment variable is:  $TERM"
+
