53cdba8b351515f8d8dad4a582966589f64550ea
Bryan Stroud <bstroud110@gmail.com>
2018-11-03 23:51:58 -0500

    Added exercise 2 from Linux Academy Bash Scripting training.

---
+++ ex2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Exercise 2 in Linux Academy Bash Scripting training
+
+MYUSERNAME="username"
+MYPASSWORD="password"
+STARTOFSCRIPT=`date`
+echo "The username is:  $MYUSERNAME"
+echo "The password is:  $MYPASSWORD"
+echo "This script started at:  $STARTOFSCRIPT"
+ENDOFSCRIPT=`date`
+echo "This script ended at: $ENDOFSCRIPT"
+
