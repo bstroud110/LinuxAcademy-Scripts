dc692fc22056413b812b853aa8f5bdef682b220f
Bryan Stroud <bstroud110@gmail.com>
2018-11-05 12:57:43 -0600

    Initial commit of script showing case example.

---
+++ casesample.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Quick demo of case statement
+
+echo "MAIN MENU"
+echo "========="
+echo "1) Choice One"
+echo "2) Choice Two"
+echo "3) Choice Three"
+echo ""
+echo "Enter choice: "
+read MENUCHOICE
+
+case $MENUCHOICE in
+  1)
+    echo "Congratulations for choosing the first option!";;
+  2)
+    echo "Choice 2 chosen.";;
+  3)
+    echo "Last choice made.";;
+  *)
+    echo "You chose poorly.";;
+esac
+
+
+
+
