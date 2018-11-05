f0d22d127dcf7f30db47cb4c0dfacd8c7f15f73e
Bryan Stroud <bstroud110@gmail.com>
2018-11-05 10:40:20 -0600

    Initial commit for nestedif.sh script.

---
+++ nestedif.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Simple Example of if then else and nested if statements
+
+echo "Enter a number between 1 and 3:"
+read VALUE
+
+if [ $VALUE -eq "1" ]
+  then
+    echo "You enered $VALUE."
+fi
+
