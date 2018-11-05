6ddd3a82e6b8cca42deac52d955ae2b82bebcc98
Bryan Stroud <bstroud110@gmail.com>
2018-11-05 09:02:00 -0600

    Added additional input values to accept for cmdline.sh.

--- cmdline.sh
+++ cmdline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Accepting input from command line
 
-echo "The following item was passed in to the script at runtime $1"
-
+echo "The following item was passed in to the script at runtime $1 $2 $3"
+ 
 
