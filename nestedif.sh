3b6ad3c2b158f54aebff7a2c106372dd167db5ef
Bryan Stroud <bstroud110@gmail.com>
2018-11-05 10:53:44 -0600

    Updated If block with differing responses based on number entered.

--- nestedif.sh
+++ nestedif.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 # Simple Example of if then else and nested if statements
 
+clear
+
 echo "Enter a number between 1 and 3:"
 read VALUE
 
-if [ "$VALUE" -eq "1" ] || [ "$VALUE" -eq 2 ] || [ "$VALUE" -eq 3 ] ; then
-  echo "You enered $VALUE."
+if [ "$VALUE" -eq "1" ] 2>/dev/null; then 
+  echo "You entered the #$VALUE."
+elif [ "$VALUE" -eq 2 ] 2>/dev/null; then
+  echo "You successfully entered #2."
+elif [ "$VALUE" -eq 3 ] 2>/dev/null; then
+  echo "You entered the third number."
+else
+  echo "You entered an invalid number!"
 fi
 
