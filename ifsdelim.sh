1d3cb43e175a3ed78c2cd15be0ae73ed8fa82733
Bryan Stroud <bstroud110@gmail.com>
2018-11-06 09:47:49 -0600

    IFS delimiter script utilizes pipedelim.txt as an example.

---
+++ ifsdelim.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Delimiter example using IFS
+
+echo "Enter file name to parse:  "
+read FILE
+
+echo "Enter the Delimiter:  "
+read DELIM
+
+IFS="$DELIM"
+
+while read -r CPU MEMORY DISK; do
+  echo "CPU:  $CPU"
+  echo "Memory:  $MEMORY"
+  echo "Disk:  $DISK"
+done < "$FILE"
+
+
